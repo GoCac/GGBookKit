84cdb4089904963a22ea1793362998cd050212e3
Youhao Gong 宫酉昊 <youhao.gong.o@nio.com>
2018-04-18 14:34:49 +0800

    0.0.1

---
+++ GGBookKit/GGBookKit.h
@@ -0,0 +1,19 @@
+//
+//  GGBookKit.h
+//  GGBookKit
+//
+//  Created by Youhao Gong 宫酉昊 on 2018/4/18.
+//  Copyright © 2018年 Youhao Gong 宫酉昊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GGBookKit.
+FOUNDATION_EXPORT double GGBookKitVersionNumber;
+
+//! Project version string for GGBookKit.
+FOUNDATION_EXPORT const unsigned char GGBookKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GGBookKit/PublicHeader.h>
+
+
---
+++ GGBookKit/Sources/SomethingController.swift
@@ -0,0 +1,23 @@
+//
+//  SomethingController.swift
+//  FDModuleMiddleware-Demo
+//
+//  Created by Youhao Gong 宫酉昊 on 2018/4/17.
+//  Copyright © 2018年 Yongpeng Zhu 朱永鹏. All rights reserved.
+//
+
+import UIKit
+
+public class SomethingController: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
